1d67b9d0c974a694eb2bb892de9a1bef91ff2287
Abdallah Abedraba <aabedraba@gmail.com>
2022-04-21 15:11:03 +0400

    Add more assertions

--- cypress/integration/simple-test.js
+++ cypress/integration/simple-test.js
@@ -1,9 +1,13 @@
-describe('The Home Page', () => {
-  it('successfully loads', () => {
-    cy.visit('https://testkube.kubeshop.io');
+describe("The Home Page", () => {
+  it("successfully loads", () => {
+    cy.visit("https://testkube.kubeshop.io");
 
     cy.contains(
-      'Testkube provides a Kubernetes-native framework for test definition, execution and results'
-    ).should("be.visible")
+      "Testkube provides a Kubernetes-native framework for test definition, execution and results"
+    ).should("be.visible");
+
+    cy.contains(
+      "Your friendly cloud-native testing framework for Kubernetes"
+    ).should("be.visible");
   });
 });
